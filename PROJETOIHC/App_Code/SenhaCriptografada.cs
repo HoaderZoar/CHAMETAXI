320bc108483425caba4bdd62c87f14974cb1edd1
WELLYSON\wellyson <we_llyson@hotmail.com>
2017-06-04 23:52:53 -0400

    envio do projeto em prototipo esperando o designer do projeto para configuração de cores e inserçoes de imagens verificar erro no cadastro de usuarios estava rodando só que modifiquei e nao me recordo caso alguem consiga.

---
+++ PROJETOIHC/App_Code/SenhaCriptografada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+/// <summary>
+/// Summary description for SenhaCriptografada
+/// </summary>
+public class SenhaCriptografada
+{
+    private string senha;
+    public override string ToString(){
+        MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+        Byte[] hashedBytes;
+        UTF8Encoding encoder = new UTF8Encoding();
+        hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(senha));
+        StringBuilder encryptedValueBuilder= new StringBuilder();
+        for (int i=0; i< hashedBytes.Length; i++){
+            encryptedValueBuilder.Append(hashedBytes[i].ToString("x2"));
+        }
+        string senha_Criptografada = encryptedValueBuilder.ToString();
+        return senha_Criptografada;
+    }
+
+    public SenhaCriptografada(string senha)
+    {
+        this.senha= senha;
+    }
+}
